22632ed0d3d92962749b7701704fb1400a7ebdab
arihito.hagiwara <arihito.hagiwara@stanby.inc>
2021-11-26 14:14:23 +0900

    一旦commit

---
+++ s3/s3.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "s3" {
+    bucket = "z-arihito-hagiwara-test" 
+    region = "us-east-1"
+    key = "s3/terraform.tfstate"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+#resource "aws_s3_bucket" "c" {
+#  bucket = "z-arihito-hagiwara-test"
+#  acl    = "private"
+#
+#  tags = {
+#    Env   = "Staging"
+#    Group = "PPG"
+#  }
+#}
